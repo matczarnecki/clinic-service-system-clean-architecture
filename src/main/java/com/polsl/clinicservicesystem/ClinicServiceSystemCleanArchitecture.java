99931f2ba743e55cbee619ad9d8429cc29ff1a8e
Mateusz Czarnecki <mateusz.czarnecki14@gmail.com>
2022-05-29 21:12:26 +0200

    fix app name

--- src/main/java/com/polsl/clinicservicesystem/ClinicServiceSystemCleanArchitecture.java
+++ src/main/java/com/polsl/clinicservicesystem/ClinicServiceSystemCleanArchitecture.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClinicServiceSystem {
+public class ClinicServiceSystemCleanArchitecture {
 
   public static void main(String[] args) {
-    SpringApplication.run(ClinicServiceSystem.class, args);
+    SpringApplication.run(ClinicServiceSystemCleanArchitecture.class, args);
   }
 
 }
