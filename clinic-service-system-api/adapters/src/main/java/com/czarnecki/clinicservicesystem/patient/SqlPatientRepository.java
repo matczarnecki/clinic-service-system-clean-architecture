d4c343006472ed5ac78b8c13c5e9cef1bb146717
Mateusz Czarnecki <mateusz.czarnecki@softiq.com>
2024-02-03 13:50:12 +0100

    feat: separate sql and domain entities for patients

--- clinic-service-system-api/adapters/src/main/java/com/czarnecki/clinicservicesystem/patient/SqlPatientRepository.java
+++ clinic-service-system-api/adapters/src/main/java/com/czarnecki/clinicservicesystem/patient/SqlPatientRepository.java
@@ -2,5 +2,38 @@ package com.czarnecki.clinicservicesystem.patient;
 
 import org.springframework.data.repository.CrudRepository;
 
-interface SqlPatientRepository extends PatientRepository, CrudRepository<Patient, Integer> {
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+interface SqlPatientRepository extends CrudRepository<SqlPatient, Integer> {
+}
+
+class PatientRepositoryImpl implements PatientRepository {
+
+    private final SqlPatientRepository repository;
+
+    public PatientRepositoryImpl(final SqlPatientRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Optional<Patient> findById(Integer id) {
+        return repository.findById(id)
+                .map(SqlPatient::toPatient);
+    }
+
+    @Override
+    public Set<Patient> findAll() {
+        return StreamSupport
+                .stream(repository.findAll().spliterator(), false)
+                .map(SqlPatient::toPatient)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Patient save(Patient entity) {
+        return repository.save(SqlPatient.fromPatient(entity)).toPatient();
+    }
 }
--- clinic-service-system-api/domain/src/main/java/com/czarnecki/clinicservicesystem/patient/Patient.java
+++ clinic-service-system-api/domain/src/main/java/com/czarnecki/clinicservicesystem/patient/Patient.java
@@ -1,21 +1,9 @@
 package com.czarnecki.clinicservicesystem.patient;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "patients")
 class Patient {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String firstName;
   private String lastName;
-
 
 
   Integer getId() {
