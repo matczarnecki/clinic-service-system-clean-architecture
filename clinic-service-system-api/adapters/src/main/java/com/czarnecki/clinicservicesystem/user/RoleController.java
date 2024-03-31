cdc1586d823e46137b3f44641ea2bad8deb25278
Mateusz Czarnecki <mateusz.czarnecki@softiq.com>
2024-03-31 22:41:11 +0200

    refactor: role controller and facade

--- clinic-service-system-api/adapters/src/main/java/com/czarnecki/clinicservicesystem/user/RoleController.java
+++ clinic-service-system-api/adapters/src/main/java/com/czarnecki/clinicservicesystem/user/RoleController.java
@@ -1,24 +1,27 @@
 package com.czarnecki.clinicservicesystem.user;
 
+import com.czarnecki.clinicservicesystem.user.query.RoleDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/api/roles")
 class RoleController {
 
-    private final RoleFacade rolesService;
+    private final RoleFacade roleFacade;
 
-    RoleController(RoleFacade rolesService) {
-        this.rolesService = rolesService;
+    RoleController(final RoleFacade roleFacade) {
+        this.roleFacade = roleFacade;
     }
 
-    @GetMapping()
+    @GetMapping
     @PreAuthorize("hasAuthority('CAN_SEE_ROLES')")
-    ResponseEntity<?> getRoles() {
-        return ResponseEntity.ok(rolesService.getRoles());
+    ResponseEntity<List<RoleDto>> getRoles() {
+        return ResponseEntity.ok(roleFacade.getRoles());
     }
 }
--- clinic-service-system-api/app/src/main/java/com/czarnecki/clinicservicesystem/user/RoleFacade.java
+++ clinic-service-system-api/app/src/main/java/com/czarnecki/clinicservicesystem/user/RoleFacade.java
@@ -1,36 +1,35 @@
 package com.czarnecki.clinicservicesystem.user;
 
 import com.czarnecki.clinicservicesystem.user.query.RoleDto;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Service;
 
 @Service
 public class RoleFacade {
+    private final RoleRepository roleRepository;
 
-  private final RoleRepository roleRepository;
+    RoleFacade(final RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
-  RoleFacade(RoleRepository roleRepository) {
-    this.roleRepository = roleRepository;
-  }
+    public List<RoleDto> getRoles() {
+        return roleRepository
+                .findAll()
+                .stream()
+                .map(this::roleToRoleDto)
+                .toList();
+    }
 
-  public List<?> getRoles() {
-    return roleRepository
-        .findAll()
-        .stream()
-        .map(this::roleToRoleDto)
-        .collect(Collectors.toList());
-  }
+    public Optional<Role> findById(String roleId) {
+        return roleRepository.findById(roleId);
+    }
 
-  public Optional<Role> findById(String roleId) {
-    return roleRepository.findById(roleId);
-  }
-
-  private RoleDto roleToRoleDto(Role role) {
-    return RoleDto.builder()
-        .withCode(role.getCode())
-        .withName(role.getName())
-        .build();
-  }
+    private RoleDto roleToRoleDto(Role role) {
+        return RoleDto.builder()
+                .withCode(role.getCode())
+                .withName(role.getName())
+                .build();
+    }
 }
